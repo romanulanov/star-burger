3453c8664f4007d6fb221ca7a3cad9e3527e43ff
romanulanov <turnsourcream@gmail.com>
2024-02-29 14:19:27 +0400

    edited fetch_coordinates

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -13,23 +13,28 @@ from django.contrib.auth import views as auth_views
 
 from foodcartapp.models import Order, Product, Restaurant, RestaurantMenuItem
 from star_burger.settings import YANDEX_KEY
+from place.models import Place
 
 def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    place = Place.objects.filter(address_place=address).first()
+    if place and place.lon and place.lat:
+        return place.lon, place.lat
+    else:
+        base_url = "https://geocode-maps.yandex.ru/1.x"
+        response = requests.get(base_url, params={
+            "geocode": address,
+            "apikey": apikey,
+            "format": "json",
+        })
+        response.raise_for_status()
+        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
 
-    if not found_places:
-        return None
+        if not found_places:
+            return None
 
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
+        most_relevant = found_places[0]
+        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+        return lon, lat
 
 
 class Login(forms.Form):
