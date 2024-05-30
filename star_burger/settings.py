9aadb50e635f7a9a0cef69b2b75e330012bb71a0
roman <turnsourcream@gmail.com>
2024-05-30 17:50:55 +0400

    Разбил профиль на отдельные настройки

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 YANDEX_KEY = os.environ['YANDEX_KEY']
-ROLLBAR_TOKEN = os.environ['ROLLBAR_TOKEN']
+ROLLBAR_TOKEN = env('ROLLBAR_TOKEN', default=None)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 DATABASE_URL = os.environ['DATABASE_URL']
 
@@ -48,7 +48,7 @@ MIDDLEWARE = [
 
 ROLLBAR = {
     'access_token': ROLLBAR_TOKEN,
-    'environment': 'development' if DEBUG else 'production',
+    'environment': env('ROLLBAR_ENVIRONMENT', default='development'),
     'code_version': '1.0',
     'root': BASE_DIR, 
 }
